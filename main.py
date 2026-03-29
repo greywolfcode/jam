0edb377992e0b1420050ecf7a62c2631bf7fc23f
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-03-28 21:55:27 -0400

    Add .m3u generation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import pathlib
 
 import scripts.jam as jam
@@ -6,31 +7,48 @@ import scripts.jam as jam
 parser = argparse.ArgumentParser(
                 prog="jam",
                 description="Creates .m3u/.m3u8 files from directory",
-                epilog="Currently only support .wav files")
+                epilog="Currently only supports .wav files")
 
 #positional arguments
 parser.add_argument("folder_path")
+parser.add_argument("playlist_name")
 
 #optional arguments
 parser.add_argument("-n", "--nonRecursive", action="store_true",
                     help="choose to not search all subdirectories")
 parser.add_argument("-t", "--type", choices=[".m3u", ".m3u8"],
-
+                    default=".m3u",
                     help="choose to output .m3u or .m3u8, .m3u is default")
+parser.add_argument("-f", "--folder", type=str, default="",
+                    help="only specified folder will be check for in subdirectories")
 
 def main(): 
     args = parser.parse_args()
 
     path = pathlib.Path(args.folder_path)
 
+
+    folder = args.folder
+    if folder != "":
+        #include any sub directories of provided folder
+        folder += os.sep + "**" + os.sep 
+
+    if args.nonRecursive:
+        music_paths = path.glob(folder + "*.wav")
+    else:
+        music_paths = path.glob("**/" + folder + "*.wav")
+
+    #mke relative paths
     
+    file = jam.create_m3u(music_paths, path)
 
+    name = args.playlist_name
+    extension = args.type
+    if args.playlist_name[-len(extension):] != extension:
+        name += extension
 
-
-
-
-
-
+    with open(os.path.join(args.folder_path, name), "w") as output:
+        output.write(file)
 
 if __name__ == "__main__":
     main()
--- scripts/jam.py
+++ scripts/jam.py
@@ -1,5 +1,31 @@
 import wave
 
-def createM3U(files):
-     output = ["#EXTM3U",""]
+def create_m3u(files, base_path):
+    """Creates m3u file from list of paths"""
 
+    output = ["#EXTM3U",""]
+
+    for file in files:
+        track = ["#EXTINF:"]
+        track.append(str(_wav_handeler(file)))
+        track.append(file.name) 
+
+        output.append(" ".join(track))
+        output.append(str(file.relative_to(base_path)))
+        output.append("")
+
+    return "\n".join(output)
+
+def _wav_handeler(path):
+    """input: file path
+       output: frame rate"""
+    
+    with wave.open(str(path), "r") as audio:
+        frame_rate = audio.getframerate()
+        total_frames = audio.getnframes()
+
+        length = total_frames / float(frame_rate)
+    return length
+        
+    
+        
