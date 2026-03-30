9270e5c13f8f2eafdeedf39ca3582e906f3f4865
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-03-29 21:42:11 -0400

    Allow multiple file types and multiple search folders to be chosen

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import os
 import pathlib
 
@@ -19,31 +20,45 @@ parser.add_argument("-n", "--nonRecursive", action="store_true",
 parser.add_argument("-t", "--type", choices=[".m3u", ".m3u8"],
                     default=".m3u",
                     help="choose to output .m3u or .m3u8, .m3u is default")
-parser.add_argument("-f", "--folder", type=str, default="",nargs="+",
+parser.add_argument("-f", "--folders", nargs="+",
                     help="only specified folder will be check for in subdirectories")
 parser.add_argument("-a", "--absolute", action="store_true",
                     help="use absolute paths")
+parser.add_argument("-m", "--music_types", nargs="*",
+                    help="select which file extensions to search")
+
+default_file_types=[".mp3", ".aac", ".m4a", ".wav", ".flac", ".aiff", ".ogg"]
 
 def main(): 
     args = parser.parse_args()
 
     path = pathlib.Path(args.folder_path)
 
-
-    folder = args.folder
-    if folder != "":
-        #include any sub directories of provided folder
-        folder += os.sep + "**" + os.sep 
-
-    if args.nonRecursive:
-        music_paths = path.glob(folder + "*.wav")
+    if args.music_types == None:
+        music_exts = default_file_types
     else:
-        music_paths = path.glob("**/" + folder + "*.wav")
+        music_exts = args.music_types
+    music_exts.sort()
+
+    paths = []
+    folders = args.folders
+    if folders == None:
+        folders = [""] #ensure there is at least 1 folder
+    folder.sort()
+    for folder in folders:
+        if folder != "":
+            #include any sub directories of provided folder
+            folder += os.sep + "**" + os.sep 
+        for ext in music_exts:
+            if args.nonRecursive:
+                paths.extend(list(path.glob(folder + "*" + ext)))
+            else:
+                paths.extend(list(path.rglob(folder + "*" + ext)))
     
     if args.absolute:
         path = ""
 
-    file = jam.create_m3u(music_paths, path)
+    file = jam.create_m3u(paths, path)
 
     name = args.playlist_name
     extension = args.type
